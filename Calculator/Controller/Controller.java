a5df0515a1d8c80c1eda1eb76fad1256da6f8c50
Ekaterina Morozova <morozova1861@mail.ru>
2023-01-18 00:10:47 +0500

    controller, model 1.3

---
+++ Calculator/Controller/Controller.java
@@ -0,0 +1,19 @@
+package Calculator.Controller;
+
+import Calculator.Model.CalcModel;
+import Calculator.Views.ConsoleView;
+
+public class Controller {
+    private CalcModel model;
+    private  ConsoleView view;
+
+    public Controller(CalcModel model, ConsoleView view) {
+        this.model = model;
+        this.view = view;
+    }
+    
+    void menu(){
+        
+    };
+    
+}
